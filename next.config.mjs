b680e9d8752ca8482a13ae9b690f9fd033059d87
nguyentruonglinh121286 <nguyentruonglinh1212@gmail.com>
2025-08-26 10:19:24 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {},
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
+
 export default nextConfig;
